9e192f03072e2defad4fdc828a6e5842cf28eddb
taodaren <taodaren1125@gmail.com>
2017-08-11 10:34:13 +0800

    使用卡片式布局完成二手交易界面

--- app/src/main/java/net/osplay/module_secondhand/SecondhandFragment.java
+++ app/src/main/java/net/osplay/module_secondhand/SecondhandFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,6 +15,10 @@ import android.widget.Toast;
 
 import net.osplay.olacos.BaseFragment;
 import net.osplay.olacos.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 /**
@@ -23,12 +29,47 @@ public class SecondhandFragment extends BaseFragment {
     //侧滑菜单
     private DrawerLayout mDrawerLayout;
 
+    //测试使用数据，有后台后可删除使用网络数据
+    private SecondhandBean[] secondhands = {
+            new SecondhandBean(R.drawable.example01, "￥356.00", "【二次元】北京奥世普雷科技有限公司怎么全是帅哥美女"),
+            new SecondhandBean(R.drawable.example02, "￥158.00", "【游戏人生剧场版】 玩家夫妇挑战了世界【MAD】"),
+            new SecondhandBean(R.drawable.example03, "￥286.00", "【花泽香菜/物语系列】千石抚子的黑与白~~"),
+            new SecondhandBean(R.drawable.example04, "￥9527.00", "【2017拜年祭单品】漫画家桐人—我只想讲一个关于梦想的故事"),
+            new SecondhandBean(R.drawable.example05, "￥945.00", "【全职高手】叶修中心向mad 这一路走来"),
+            new SecondhandBean(R.drawable.example06, "￥1725.00", "[CLANNAD/MAD]这段人生已走过十年，而我们将用更多十年走完各自的人生"),
+            new SecondhandBean(R.drawable.example07, "￥365.00", "【全职高手】1025荣耀学院"),
+            new SecondhandBean(R.drawable.example08, "￥131.00", "【MAD】 聖なるかな 永遠になるかな CompleteVer"),
+            new SecondhandBean(R.drawable.example09, "￥186.00", "【ToLove】我的后宫不可能都是狐狸精！"),
+            new SecondhandBean(R.drawable.example10, "￥1121.00", "【腐向】一言不合就发车 奉上你们想要的《K》"),
+    };
+
+    private List<SecondhandBean> beanList = new ArrayList<>();
+    private SecondhandAdapter adapter;
+
     @Override
     public View initView() {
         View inflate = View.inflate(getContext(), R.layout.fragment_secondhand, null);
         //注意 getActivity() 若使用 view 会报错，此处有大坑
         mDrawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+
+        initSecondhands();
+        RecyclerView recyclerView = (RecyclerView) inflate.findViewById(R.id.recycler_secondhand);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new SecondhandAdapter(beanList);
+        recyclerView.setAdapter(adapter);
         return inflate;
+    }
+
+    private void initSecondhands() {
+        //清空数据
+        beanList.clear();
+        //模拟数据
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(secondhands.length);
+            beanList.add(secondhands[index]);
+        }
     }
 
     /**
