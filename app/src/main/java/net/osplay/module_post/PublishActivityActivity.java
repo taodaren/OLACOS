f8b84bf3ee1fcb9270936cb05d3ad9801ea45866
taodaren <taodaren1125@gmail.com>
2017-08-11 16:35:38 +0800

    设置发布模块 Toolbar

--- app/src/main/java/net/osplay/module_post/PublishActivityActivity.java
+++ app/src/main/java/net/osplay/module_post/PublishActivityActivity.java
@@ -1,15 +1,29 @@
 package net.osplay.module_post;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import net.osplay.olacos.BaseActivity;
 import net.osplay.olacos.R;
 
-public class PublishActivityActivity extends Activity {
+public class PublishActivityActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_activity);
+        setToolbar("发布活动", View.VISIBLE);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/net/osplay/module_post/PublishGoodsActivity.java
+++ app/src/main/java/net/osplay/module_post/PublishGoodsActivity.java
@@ -1,15 +1,28 @@
 package net.osplay.module_post;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import net.osplay.olacos.BaseActivity;
 import net.osplay.olacos.R;
 
-public class PublishGoodsActivity extends Activity {
+public class PublishGoodsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_goods);
+        setToolbar("发布商品", View.VISIBLE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 }
