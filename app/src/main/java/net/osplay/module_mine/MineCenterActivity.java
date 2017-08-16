c64eefff88ea1352fd0f4ac50ca8a3d04b695491
taodaren <taodaren1125@gmail.com>
2017-08-16 12:58:49 +0800

    优化个人中心布局及动态代码填写

--- app/src/main/java/net/osplay/module_mine/MineCenterActivity.java
+++ app/src/main/java/net/osplay/module_mine/MineCenterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import net.osplay.olacos.R;
 import net.osplay.olacos.base.BaseActivity;
@@ -15,7 +16,7 @@ import net.osplay.olacos.base.BaseActivity;
  * 个人中心
  */
 
-public class MineCenterActivity extends BaseActivity {
+public class MineCenterActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MineCenterActivity extends BaseActivity {
         setContentView(R.layout.activity_mine_center);
 
         setToolbar();
+        setClickListener();
     }
 
     private void setToolbar() {
@@ -37,6 +39,26 @@ public class MineCenterActivity extends BaseActivity {
         }
         //隐藏 CollapsingToolbarLayout 标题
         collapsingToolbar.setTitleEnabled(false);
+    }
+
+    private void setClickListener() {
+        findViewById(R.id.mine_avatar).setOnClickListener(this);
+        findViewById(R.id.mine_praise).setOnClickListener(this);
+        findViewById(R.id.mine_follow).setOnClickListener(this);
+        findViewById(R.id.mine_fans).setOnClickListener(this);
+        findViewById(R.id.mine_column).setOnClickListener(this);
+        findViewById(R.id.mine_word).setOnClickListener(this);
+        findViewById(R.id.mine_league).setOnClickListener(this);
+        findViewById(R.id.mine_money).setOnClickListener(this);
+        findViewById(R.id.mine_bao_xiang).setOnClickListener(this);
+        findViewById(R.id.mine_fa_huo).setOnClickListener(this);
+        findViewById(R.id.mine_shou_huo).setOnClickListener(this);
+        findViewById(R.id.mine_ping_jia).setOnClickListener(this);
+        findViewById(R.id.mine_tui_huo).setOnClickListener(this);
+        findViewById(R.id.mine_fa_bu).setOnClickListener(this);
+        findViewById(R.id.mine_mai_chu).setOnClickListener(this);
+        findViewById(R.id.mine_mai_dao).setOnClickListener(this);
+        findViewById(R.id.mine_zan_guo).setOnClickListener(this);
     }
 
     /**
@@ -65,5 +87,62 @@ public class MineCenterActivity extends BaseActivity {
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mine_avatar:
+
+                break;
+            case R.id.mine_praise:
+
+                break;
+            case R.id.mine_follow:
+
+                break;
+            case R.id.mine_fans:
+
+                break;
+            case R.id.mine_column:
+
+                break;
+            case R.id.mine_word:
+
+                break;
+            case R.id.mine_league:
+
+                break;
+            case R.id.mine_money:
+
+                break;
+            case R.id.mine_bao_xiang:
+
+                break;
+            case R.id.mine_fa_huo:
+
+                break;
+            case R.id.mine_shou_huo:
+
+                break;
+            case R.id.mine_ping_jia:
+
+                break;
+            case R.id.mine_tui_huo:
+
+                break;
+            case R.id.mine_fa_bu:
+
+                break;
+            case R.id.mine_mai_chu:
+
+                break;
+            case R.id.mine_mai_dao:
+
+                break;
+            case R.id.mine_zan_guo:
+
+                break;
+        }
     }
 }
