7176cc5c96c53b3aa9ec4eda1bc5b90c487ea0d2
taodaren <taodaren1125@gmail.com>
2017-08-28 17:33:14 +0800

    添加实名认证界面，完成跳转

--- app/src/main/java/net/osplay/ui/activity/EditInfoActivity.java
+++ app/src/main/java/net/osplay/ui/activity/EditInfoActivity.java
@@ -1,11 +1,12 @@
 package net.osplay.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import net.osplay.olacos.R;
 
-public class EditInfoActivity extends BaseActivity {
+public class EditInfoActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +14,15 @@ public class EditInfoActivity extends BaseActivity {
         setContentView(R.layout.activity_edit_info);
 
         setToolbar("编辑资料", View.VISIBLE);
+        findViewById(R.id.edit_info_real_name).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.edit_info_real_name:
+                startActivity(new Intent(EditInfoActivity.this, EditRealNameActivity.class));
+                break;
+        }
+    }
 }
