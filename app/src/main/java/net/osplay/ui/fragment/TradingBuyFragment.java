d4fbf98d8183208be73b54fa6c51ac234ff2eecc
taodaren <taodaren1125@gmail.com>
2017-08-29 16:37:06 +0800

    创建买到、卖出碎片

---
+++ app/src/main/java/net/osplay/ui/fragment/TradingBuyFragment.java
@@ -0,0 +1,24 @@
+package net.osplay.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.osplay.olacos.R;
+
+/**
+ * 宅配宝箱 → 我卖出的
+ */
+
+public class TradingBuyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trading_buy, container, false);
+    }
+
+}
---
+++ app/src/main/java/net/osplay/ui/fragment/TradingSellFragment.java
@@ -0,0 +1,24 @@
+package net.osplay.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.osplay.olacos.R;
+
+/**
+ * 宅配宝箱 → 我买到的
+ */
+
+public class TradingSellFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trading_sell, container, false);
+    }
+
+}
