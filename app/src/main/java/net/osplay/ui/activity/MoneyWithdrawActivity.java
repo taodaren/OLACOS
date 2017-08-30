4e288325b1ac2820d77967712f2453a475e0126b
taodaren <taodaren1125@gmail.com>
2017-08-30 13:01:18 +0800

    零钱提现 界面设计、布局、逻辑跳转

--- app/src/main/java/net/osplay/ui/activity/MoneyWithdrawActivity.java
+++ app/src/main/java/net/osplay/ui/activity/MoneyWithdrawActivity.java
@@ -1,7 +1,9 @@
 package net.osplay.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import net.osplay.olacos.R;
 
@@ -9,11 +11,32 @@ import net.osplay.olacos.R;
  * 钱包 → 提现界面
  */
 
-public class MoneyWithdrawActivity extends AppCompatActivity {
+public class MoneyWithdrawActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_withdraw);
+        initView();
     }
+
+    private void initView() {
+        setToolbar("零钱提现", View.VISIBLE);
+        findViewById(R.id.text_to_card).setOnClickListener(this);
+        findViewById(R.id.btn_money_withdraw).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_to_card:
+                startActivity(new Intent(MoneyWithdrawActivity.this, MoneySelectCardActivity.class));
+                break;
+            case R.id.btn_money_withdraw:
+                Toast.makeText(this, "进入提现逻辑", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+    }
+
 }
