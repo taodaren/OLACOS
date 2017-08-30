4febb925cca9f7c142bb8c20db325636fcbbef9e
taodaren <taodaren1125@gmail.com>
2017-08-30 10:37:24 +0800

    Create 充值、提现 Activity

---
+++ app/src/main/java/net/osplay/ui/activity/MoneyValueActivity.java
@@ -0,0 +1,19 @@
+package net.osplay.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.osplay.olacos.R;
+
+/**
+ * 钱包 → 充值界面
+ */
+
+public class MoneyValueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_value);
+    }
+}
---
+++ app/src/main/java/net/osplay/ui/activity/MoneyWithdrawActivity.java
@@ -0,0 +1,19 @@
+package net.osplay.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.osplay.olacos.R;
+
+/**
+ * 钱包 → 提现界面
+ */
+
+public class MoneyWithdrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_withdraw);
+    }
+}
