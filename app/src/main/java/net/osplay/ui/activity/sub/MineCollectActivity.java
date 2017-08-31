2077f62177c080951776618be607e9005cc0b62f
taodaren <taodaren1125@gmail.com>
2017-08-31 11:13:08 +0800

    我的发布、我的收藏改为 TabLayout 形式

--- app/src/main/java/net/osplay/ui/activity/sub/MineCollectActivity.java
+++ app/src/main/java/net/osplay/ui/activity/sub/MineCollectActivity.java
@@ -1,17 +1,27 @@
 package net.osplay.ui.activity.sub;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.view.View;
 
 import net.osplay.olacos.R;
 import net.osplay.ui.activity.base.BaseActivity;
+import net.osplay.ui.fragment.sub.ItemGoodsFragment;
+import net.osplay.ui.fragment.sub.ItemPostsFragment;
+import net.osplay.ui.fragment.sub.TradingBuyFragment;
+import net.osplay.ui.fragment.sub.TradingSellFragment;
 
 /**
  * 个人中心 → 我的收藏
  */
 
 public class MineCollectActivity extends BaseActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,36 @@ public class MineCollectActivity extends BaseActivity {
 
     private void initView() {
         setToolbar("收藏列表", View.VISIBLE);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout_collect);
+        viewPager = (ViewPager) findViewById(R.id.vp_collect);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            String[] itemName = {"商品", "热帖"};
+
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new ItemGoodsFragment();
+                    case 1:
+                        return new ItemPostsFragment();
+                }
+                return new ItemGoodsFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return itemName.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return itemName[position];
+            }
+        });
+
+        //绑定
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
