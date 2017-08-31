34479cf31f8b438dd57167f50864eabc79873173
jiguobin <1176780992@qq.com>
2017-08-31 16:45:30 +0800

    提交成功后sp记录创建社团状态

---
+++ app/src/main/java/net/osplay/ui/activity/sub/CreateCommunityActivity.java
@@ -0,0 +1,62 @@
+package net.osplay.ui.activity.sub;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import net.osplay.olacos.R;
+import net.osplay.ui.activity.base.BaseActivity;
+
+public class CreateCommunityActivity extends BaseActivity {
+
+    private EditText create_community_ed, create_area_ed, create_reason_ed;
+    private Button create_submit_btn;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_community);
+        preferences=getSharedPreferences("CreateCommunity",Activity.MODE_PRIVATE);
+        editor=preferences.edit();
+        setToolbar("创建社团", View.VISIBLE);
+        initView();
+        bindEvent();
+    }
+
+    private void initView() {
+        create_community_ed = (EditText) findViewById(R.id.create_community_ed);
+        create_area_ed = (EditText) findViewById(R.id.create_area_ed);
+        create_reason_ed = (EditText) findViewById(R.id.create_reason_ed);
+
+        create_submit_btn = (Button) findViewById(R.id.create_submit_btn);
+    }
+
+    private void bindEvent() {
+        create_submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(create_community_ed.getText().toString().isEmpty()){
+                    Toast.makeText(CreateCommunityActivity.this,"请输入社团名称",Toast.LENGTH_SHORT).show();
+                }else if(create_area_ed.getText().toString().isEmpty()){
+                    Toast.makeText(CreateCommunityActivity.this,"请输入地区",Toast.LENGTH_SHORT).show();
+                }else if(create_reason_ed.getText().toString().isEmpty()){
+                    Toast.makeText(CreateCommunityActivity.this,"请输入申团理由",Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(new Intent(CreateCommunityActivity.this,MainActivity.class));
+                    finish();
+                    editor.putString("Annotated", "olacos");
+                    editor.commit();
+                }
+            }
+        });
+    }
+
+
+}
