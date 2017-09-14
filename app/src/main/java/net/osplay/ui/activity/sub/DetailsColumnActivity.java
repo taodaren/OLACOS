8e56ca40a843533b7764db38c1f89a376b04d262
taodaren <taodaren1125@gmail.com>
2017-09-14 09:48:09 +0800

    专栏详情动态代码实现

--- app/src/main/java/net/osplay/ui/activity/sub/DetailsColumnActivity.java
+++ app/src/main/java/net/osplay/ui/activity/sub/DetailsColumnActivity.java
@@ -1,19 +1,76 @@
 package net.osplay.ui.activity.sub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import net.osplay.olacos.R;
+import net.osplay.ui.activity.base.BaseActivity;
+import net.osplay.ui.adapter.TabViewPagerAdapter;
+import net.osplay.ui.fragment.sub.WordHotPostsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 专栏某一专栏详情
  */
 
-public class DetailsColumnActivity extends AppCompatActivity {
+public class DetailsColumnActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_column);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.btn_column_details_subscribe).setOnClickListener(this);
+
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_column_details);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_column_details);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //隐藏 Toolbar 自带标题栏
+            actionBar.setDisplayShowTitleEnabled(false);
+            //设置侧滑导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.drawable.title_back);
+        }
+        //隐藏 CollapsingToolbarLayout 标题
+        collapsingToolbar.setTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_column_details_subscribe://订阅
+                Toast.makeText(this, "btn_column_details_subscribe", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
