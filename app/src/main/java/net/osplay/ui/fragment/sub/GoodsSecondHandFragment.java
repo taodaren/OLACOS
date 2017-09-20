160b5a133e735ac54f9124767b63e652bb1381a4
jiguobin1 <1176780992qq>
2017-09-20 11:14:26 +0800

    合并代码

--- app/src/main/java/net/osplay/ui/fragment/sub/GoodsSecondHandFragment.java
+++ app/src/main/java/net/osplay/ui/fragment/sub/GoodsSecondHandFragment.java
@@ -1,21 +1,17 @@
 package net.osplay.ui.fragment.sub;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import net.osplay.olacos.R;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GoodsSecondHandFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
