215d0402672fb34a3b572ecce62d2e2a2f90a127
taodaren <taodaren1125@gmail.com>
2017-09-30 07:36:40 +0800

    处理帖子详情点击事件

--- app/src/main/java/net/osplay/ui/adapter/sub/WordTopicListAdapter.java
+++ app/src/main/java/net/osplay/ui/adapter/sub/WordTopicListAdapter.java
@@ -6,12 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import net.osplay.app.AppHelper;
 import net.osplay.app.I;
+import net.osplay.app.MFGT;
 import net.osplay.olacos.R;
 import net.osplay.service.entity.WordTopicListBean;
 
@@ -34,24 +37,6 @@ public class WordTopicListAdapter extends RecyclerView.Adapter<RecyclerView.View
         this.mRowsBeanList.addAll(rowsBeanList);
     }
 
-    @Override
-    public PostsInfoAllHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View inflate = mInflater.inflate(R.layout.item_word_topic_list, parent, false);
-        PostsInfoAllHolder holder = new PostsInfoAllHolder(inflate);
-        holder.setClickListener();
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ((PostsInfoAllHolder) holder).bindData(position);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mRowsBeanList == null ? 0 : mRowsBeanList.size();
-    }
-
     public void setData(List<WordTopicListBean.RowsBean> data) {
         if (mRowsBeanList != null) {
             mRowsBeanList.clear();
@@ -62,22 +47,46 @@ public class WordTopicListAdapter extends RecyclerView.Adapter<RecyclerView.View
         notifyDataSetChanged();
     }
 
-    private class PostsInfoAllHolder extends RecyclerView.ViewHolder {
-        private View outView;
-        private TextView tvNick, tvTime, tvTitle, tvZan, tvCollect, tvComment;
-        private ImageView imgAvatar, imgBg;
+    @Override
+    public PostsInfoListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        PostsInfoListHolder holder = new PostsInfoListHolder(mInflater.inflate(R.layout.item_word_topic_list, parent, false));
+        holder.setClickListener();
+        return holder;
+    }
 
-        private PostsInfoAllHolder(View itemView) {
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((PostsInfoListHolder) holder).bindData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRowsBeanList == null ? 0 : mRowsBeanList.size();
+    }
+
+    private class PostsInfoListHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private int flag;
+        private View outView;
+        private LinearLayout llZan, llCollect, llComment;
+        private TextView tvNick, tvTime, tvTitle, tvZan, tvCollect, tvComment;
+        private ImageView imgAvatar, imgBg, imgZan, imgCollect;
+
+        private PostsInfoListHolder(View itemView) {
             super(itemView);
             outView = itemView;
-            tvNick = (TextView) itemView.findViewById(R.id.item_topic_all_nick);
-            tvTime = (TextView) itemView.findViewById(R.id.item_topic_all_time);
-            tvTitle = (TextView) itemView.findViewById(R.id.item_topic_all_title);
-            tvZan = (TextView) itemView.findViewById(R.id.item_topic_all_good);
-            tvCollect = (TextView) itemView.findViewById(R.id.item_topic_all_collect);
-            tvComment = (TextView) itemView.findViewById(R.id.item_topic_all_comment);
-            imgAvatar = (ImageView) itemView.findViewById(R.id.item_topic_all_avatar);
-            imgBg = (ImageView) itemView.findViewById(R.id.item_topic_all_icon);
+            tvNick = (TextView) itemView.findViewById(R.id.tv_topic_list_nick);
+            tvTime = (TextView) itemView.findViewById(R.id.tv_topic_list_time);
+            tvTitle = (TextView) itemView.findViewById(R.id.tv_topic_list_title);
+            tvZan = (TextView) itemView.findViewById(R.id.tv_topic_list_zan);
+            tvCollect = (TextView) itemView.findViewById(R.id.tv_topic_list_collect);
+            tvComment = (TextView) itemView.findViewById(R.id.tv_topic_list_comment);
+            imgAvatar = (ImageView) itemView.findViewById(R.id.img_topic_list_avatar);
+            imgBg = (ImageView) itemView.findViewById(R.id.img_topic_list_icon);
+            imgZan = (ImageView) itemView.findViewById(R.id.img_topic_list_zan);
+            imgCollect = (ImageView) itemView.findViewById(R.id.img_topic_list_collect);
+            llZan = (LinearLayout) itemView.findViewById(R.id.ll_topic_list_zan);
+            llCollect = (LinearLayout) itemView.findViewById(R.id.ll_topic_list_collect);
+            llComment = (LinearLayout) itemView.findViewById(R.id.ll_topic_list_comment);
         }
 
         public void bindData(int position) {
@@ -97,12 +106,55 @@ public class WordTopicListAdapter extends RecyclerView.Adapter<RecyclerView.View
         }
 
         public void setClickListener() {
-            outView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(mContext, "outView", Toast.LENGTH_SHORT).show();
-                }
-            });
+            outView.setOnClickListener(this);
+            llZan.setOnClickListener(this);
+            llCollect.setOnClickListener(this);
+            llComment.setOnClickListener(this);
+            imgAvatar.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.img_topic_list_avatar:
+                    Toast.makeText(mContext, "click Avatar", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.ll_topic_list_zan://点赞
+                    if (!(AppHelper.getInstance().isLogined())) {
+                        MFGT.gotoLogin(mContext, "loginHeck");
+                    } else {
+                        if (flag == 0) {
+                            imgZan.setImageResource(R.drawable.ic_sugar_selected);
+                            Toast.makeText(mContext, "zan+1", Toast.LENGTH_SHORT).show();
+                        } else if (flag == 1) {
+                            imgZan.setImageResource(R.drawable.ic_sugar_unselected);
+                            Toast.makeText(mContext, "zan-1", Toast.LENGTH_SHORT).show();
+                        }
+                        flag = (flag + 1) % 2;
+                    }
+                    break;
+                case R.id.ll_topic_list_collect://收藏
+                    if (!(AppHelper.getInstance().isLogined())) {
+                        MFGT.gotoLogin(mContext, "loginHeck");
+                    } else {
+                        if (flag == 0) {
+                            imgCollect.setImageResource(R.drawable.ic_collect_selected);
+                            Toast.makeText(mContext, "collect+1", Toast.LENGTH_SHORT).show();
+                        } else if (flag == 1) {
+                            imgCollect.setImageResource(R.drawable.ic_collect_unselected);
+                            Toast.makeText(mContext, "collect-1", Toast.LENGTH_SHORT).show();
+                        }
+                        flag = (flag + 1) % 2;
+                    }
+                    break;
+                case R.id.ll_topic_list_comment://评论
+                    Toast.makeText(mContext, "click comment", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(mContext, "click outView", Toast.LENGTH_SHORT).show();
+                    break;
+            }
         }
     }
+
 }
