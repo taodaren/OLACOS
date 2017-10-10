6f9c236a6b9b9cdb5459709d660472716931fcf5
taodaren <taodaren1125@gmail.com>
2017-10-10 14:37:06 +0800

    专题某一分区详情梳理

--- app/src/main/java/net/osplay/ui/activity/sub/DetailsTopicActivity.java
+++ app/src/main/java/net/osplay/ui/activity/sub/DetailsTopicActivity.java
@@ -41,6 +41,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
@@ -48,38 +49,44 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class DetailsTopicActivity extends BaseActivity implements View.OnClickListener {
-    @BindView(R.id.topic_details_nick)
-    TextView topicDetailsNick;
-    @BindView(R.id.tv_topic_details_level)
-    TextView tvTopicDetailsLevel;
-    @BindView(R.id.pb_topic_details_level)
-    ProgressBar progressBar;
-    @BindView(R.id.topic_page_avatar)
-    CircleImageView topicPageAvatar;
-    @BindView(R.id.btn_topic_heck_in)
-    Button btnHeckIn;
-    @BindView(R.id.btn_topic_attention)
-    Button btnAttention;
+    private static final String TAG = "DetailsTopicActivity";
+    @BindView(R.id.topic_details_nick) TextView tvNick;
+    @BindView(R.id.tv_topic_details_level) TextView tvLevel;
+    @BindView(R.id.pb_topic_details_level) ProgressBar progressBar;
+    @BindView(R.id.topic_page_avatar) CircleImageView imgAvatar;
+    @BindView(R.id.btn_topic_heck_in) Button btnHeckIn;
+    @BindView(R.id.btn_topic_attention) Button btnAttention;
 
+    private Unbinder unbinder;
+    private int flag;
     private ViewPager mViewPager;
     private Gson gson = new Gson();
-    private int flag;
     private String partId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_topic);
-        ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
         initData();
         initView();
+    }
+
+    private void initData() {
+        partId = getIntent().getStringExtra("partId");//获取大区 id
+
+        RequestQueue requestQueue = NoHttp.newRequestQueue();
+        Request<String> topicRequest = NoHttp.createStringRequest(I.AREA_SUB, RequestMethod.POST);
+        topicRequest.add("partId", partId);
+
+        getDetailsTopicData(requestQueue, topicRequest);//请求专区数据
     }
 
     private void initView() {
         setToolbar();
         btnHeckIn.setOnClickListener(this);
         btnAttention.setOnClickListener(this);
-        topicPageAvatar.setOnClickListener(this);
+        imgAvatar.setOnClickListener(this);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout_topic_details);
         mViewPager = (ViewPager) findViewById(R.id.vp_topic_details);
@@ -93,107 +100,26 @@ public class DetailsTopicActivity extends BaseActivity implements View.OnClickLi
     }
 
     /**
-     * 未登录状态显示专区图片和信息
+     * 通过登录状态改变 View
      */
     private void changeViewByState() {
-        if (!AppHelper.getInstance().isLogined()) {
-            tvTopicDetailsLevel.setVisibility(View.GONE);
+        if (!AppHelper.getInstance().isLogined()) {//未登录状态
+            tvLevel.setVisibility(View.GONE);
             progressBar.setVisibility(View.GONE);
             Intent intent = getIntent();
             if (intent != null) {
                 String title = intent.getStringExtra(I.Type.TYPE_NAME);
-                topicDetailsNick.setText(title);
+                tvNick.setText(title);
                 int imgId = intent.getIntExtra(I.Img.IMG_KEY, 0);
-                Glide.with(DetailsTopicActivity.this).load(imgId).into(topicPageAvatar);
+                Glide.with(DetailsTopicActivity.this).load(imgId).into(imgAvatar);
             }
-        } else {
-            tvTopicDetailsLevel.setVisibility(View.VISIBLE);
+        } else {//登录状态
+            tvLevel.setVisibility(View.VISIBLE);
             progressBar.setVisibility(View.VISIBLE);
             // set Avatar and Nick
-            Glide.with(DetailsTopicActivity.this).load(I.BASE_URL + AppHelper.getInstance().getUser().getHEAD_PATH()).into(topicPageAvatar);
-            topicDetailsNick.setText(AppHelper.getInstance().getUser().getNICK_NAME());
+            Glide.with(DetailsTopicActivity.this).load(I.BASE_URL + AppHelper.getInstance().getUser().getHEAD_PATH()).into(imgAvatar);
+            tvNick.setText(AppHelper.getInstance().getUser().getNICK_NAME());
         }
-    }
-
-    private void initData() {
-        partId = getIntent().getStringExtra("partId");//获取大区 id
-
-        RequestQueue requestQueue = NoHttp.newRequestQueue();
-        Request<String> topicRequest = NoHttp.createStringRequest(I.AREA_SUB, RequestMethod.POST);
-        topicRequest.add("partId", partId);
-
-        getDetailsTopicData(requestQueue, topicRequest);//帖子内容
-    }
-
-    private void getDetailsTopicData(RequestQueue requestQueue, Request<String> request) {
-        requestQueue.add(0, request, new OnResponseListener<String>() {
-            @Override
-            public void onStart(int what) {
-            }
-
-            @Override
-            public void onSucceed(int what, Response<String> response) {
-                String json = response.get();//得到请求数据
-                if (json != null) {
-                    Type type = new TypeToken<List<WordTopicTitleBean>>() {
-                    }.getType();
-                    List<WordTopicTitleBean> titleBeanList = gson.fromJson(json, type);
-                    setViewPager(titleBeanList);
-                }
-            }
-
-            @Override
-            public void onFailed(int what, Response<String> response) {
-            }
-
-            @Override
-            public void onFinish(int what) {
-            }
-        });
-    }
-
-    private void setViewPager(final List<WordTopicTitleBean> wordTopicList) {
-        //设置标题个数和值以及 fragment 的对应个数
-        String[] areaArr = new String[wordTopicList.size()];
-
-        for (int i = 0; i < wordTopicList.size(); i++) {
-            areaArr[i] = wordTopicList.get(i).getPART();//获取专区名
-        }
-
-        List<Fragment> mFragmentList = new ArrayList<>();
-        for (int i = 0; i < wordTopicList.size(); i++) {
-            mFragmentList.add(DetailsTopicInfoFragment.newInstance(wordTopicList.get(i).getID()));//对应专区添加布局
-        }
-
-        TabViewPagerAdapter mAdapter = new TabViewPagerAdapter(getSupportFragmentManager(), this, mFragmentList, areaArr);
-        mViewPager.setAdapter(mAdapter);
-    }
-
-    private void setToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_topic_page);
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_topic);
-
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            //隐藏 Toolbar 自带标题栏
-            actionBar.setDisplayShowTitleEnabled(false);
-            //设置侧滑导航按钮图标
-            actionBar.setHomeAsUpIndicator(R.drawable.title_back);
-        }
-        //隐藏 CollapsingToolbarLayout 标题
-        collapsingToolbar.setTitleEnabled(false);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return true;
     }
 
     @Override
@@ -222,13 +148,13 @@ public class DetailsTopicActivity extends BaseActivity implements View.OnClickLi
                     MFGT.gotoLogin(this, "loginAttention");
                 } else {
                     if (flag == 0) {
-                        attentionHttp();
                         btnAttention.setText("已关注");
                         btnAttention.setBackgroundResource(R.drawable.shape_yuan_trans);
+                        getAttentionData();//关注数据请求解析
                     } else if (flag == 1) {
-                        unAttentionHttp();
                         btnAttention.setText("关注专区");
                         btnAttention.setBackgroundResource(R.drawable.shape_yuan);
+                        getUnAttentionData();//取消关注数据请求解析
                     }
                     flag = (flag + 1) % 2;
                 }
@@ -236,10 +162,51 @@ public class DetailsTopicActivity extends BaseActivity implements View.OnClickLi
         }
     }
 
-    /**
-     * 取消关注
-     */
-    private void unAttentionHttp() {
+    private void getDetailsTopicData(RequestQueue requestQueue, Request<String> request) {
+        requestQueue.add(0, request, new OnResponseListener<String>() {
+            @Override
+            public void onStart(int what) {
+            }
+
+            @Override
+            public void onSucceed(int what, Response<String> response) {
+                String json = response.get();//得到请求数据
+                if (json != null) {
+                    Type type = new TypeToken<List<WordTopicTitleBean>>() {
+                    }.getType();
+                    List<WordTopicTitleBean> titleBeanList = gson.fromJson(json, type);
+                    setViewPager(titleBeanList);
+                }
+            }
+
+            @Override
+            public void onFailed(int what, Response<String> response) {
+            }
+
+            @Override
+            public void onFinish(int what) {
+            }
+
+            private void setViewPager(List<WordTopicTitleBean> wordTopicList) {
+                //设置标题个数和值以及 fragment 的对应个数
+                String[] areaArr = new String[wordTopicList.size()];
+
+                for (int i = 0; i < wordTopicList.size(); i++) {
+                    areaArr[i] = wordTopicList.get(i).getPART();//获取专区名
+                }
+
+                List<Fragment> mFragmentList = new ArrayList<>();
+                for (int i = 0; i < wordTopicList.size(); i++) {
+                    mFragmentList.add(DetailsTopicInfoFragment.newInstance(wordTopicList.get(i).getID()));//对应专区添加布局
+                }
+
+                TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager(), DetailsTopicActivity.this, mFragmentList, areaArr);
+                mViewPager.setAdapter(adapter);
+            }
+        });
+    }
+
+    private void getUnAttentionData() {
         RequestQueue requestQueue = NoHttp.newRequestQueue();
         Request<String> request = NoHttp.createStringRequest(I.ATTENORCANCEL, RequestMethod.POST);
         request.add("memberId", AppHelper.getInstance().getUser().getID());
@@ -270,10 +237,7 @@ public class DetailsTopicActivity extends BaseActivity implements View.OnClickLi
         });
     }
 
-    /**
-     * 关注
-     */
-    private void attentionHttp() {
+    private void getAttentionData() {
         RequestQueue requestQueue = NoHttp.newRequestQueue();
         Request<String> request = NoHttp.createStringRequest(I.ATTENORCANCEL, RequestMethod.POST);
         request.add("memberId", AppHelper.getInstance().getUser().getID());
@@ -302,6 +266,39 @@ public class DetailsTopicActivity extends BaseActivity implements View.OnClickLi
             public void onFinish(int what) {
             }
         });
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_topic_page);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_topic);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //隐藏 Toolbar 自带标题栏
+            actionBar.setDisplayShowTitleEnabled(false);
+            //设置侧滑导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.drawable.title_back);
+        }
+        //隐藏 CollapsingToolbarLayout 标题
+        collapsingToolbar.setTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 
 }
