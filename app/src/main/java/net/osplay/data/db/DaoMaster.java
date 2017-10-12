e96818fa6cbad63e0e654231461d2d6d73ffcdba
jiguobin1 <1176780992qq>
2017-10-12 16:44:20 +0800

    使用greendao创建community库

--- app/src/main/java/net/osplay/data/db/DaoMaster.java
+++ app/src/main/java/net/osplay/data/db/DaoMaster.java
@@ -22,11 +22,13 @@ public class DaoMaster extends AbstractDaoMaster {
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
         AccountDao.createTable(db, ifNotExists);
+        CommunityDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
         AccountDao.dropTable(db, ifExists);
+        CommunityDao.dropTable(db, ifExists);
     }
 
     /**
@@ -46,6 +48,7 @@ public class DaoMaster extends AbstractDaoMaster {
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
         registerDaoClass(AccountDao.class);
+        registerDaoClass(CommunityDao.class);
     }
 
     public DaoSession newSession() {
