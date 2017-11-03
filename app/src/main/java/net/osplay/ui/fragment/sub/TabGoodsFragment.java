25a5c16a1d5a0a6a4290962e98ba7042a2e7b805
taodaren <taodaren1125@gmail.com>
2017-11-03 15:47:04 +0800

    初始化商品模块 Toolbar，并添加搜索功能跳转

--- app/src/main/java/net/osplay/ui/fragment/sub/TabGoodsFragment.java
+++ app/src/main/java/net/osplay/ui/fragment/sub/TabGoodsFragment.java
@@ -1,8 +1,13 @@
 package net.osplay.ui.fragment.sub;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
@@ -10,6 +15,7 @@ import android.webkit.WebViewClient;
 
 import net.osplay.app.I;
 import net.osplay.olacos.R;
+import net.osplay.ui.activity.sub.SearchActivity;
 import net.osplay.ui.fragment.base.BaseFragment;
 
 /**
@@ -36,12 +42,39 @@ public class TabGoodsFragment extends BaseFragment {
         return inflate;
     }
 
+    /**
+     * 在 onActivityCreated 方法中初始化 Toolbar
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setToolbar(R.id.toolbar_goods, R.string.goods_name, View.VISIBLE, View.GONE, true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //显示菜单
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        //显示需要菜单项，隐藏多余菜单项
+        menu.findItem(R.id.menu_msg).setVisible(false);
+        menu.findItem(R.id.menu_search).setVisible(true);
+        menu.findItem(R.id.menu_code).setVisible(false);
+        menu.findItem(R.id.menu_category).setVisible(false);
+        menu.findItem(R.id.menu_register).setVisible(false);
+        menu.findItem(R.id.menu_set).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home://导航按钮固定 id
                 //展示滑动菜单
                 mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.menu_search:
+                //跳转搜索界面
+                startActivity(new Intent(getContext(), SearchActivity.class));
                 break;
         }
         return true;
