43b41396d858ba8c55cd2b1530d2434dc9fb60b8
jiguobin1 <1176780992qq>
2017-11-10 20:31:54 +0800

    完成关于我们

---
+++ app/src/main/java/net/osplay/ui/activity/sub/OlacosActivity.java
@@ -0,0 +1,15 @@
+package net.osplay.ui.activity.sub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.osplay.olacos.R;
+
+public class OlacosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olacos);
+    }
+}
